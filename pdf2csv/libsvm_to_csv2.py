09f5c96d209b701102b1e642c795f39e49418dcd
yujinchang804 <93634770+yujinchang804@users.noreply.github.com>
2021-11-28 10:27:23 +0900

    28_1027_YJ

--- pdf2csv/libsvm_to_csv2.py
+++ pdf2csv/libsvm_to_csv2.py
@@ -14,8 +14,11 @@ for line in lines:
 			key = int(l[i].split(':')[0])
 			val = float(l[i].split(':')[1])
 			minus_list[key-1]=val
+			
+	# 교수님 코드에서는 if l[0]값에 따라 mb = 'M' or mb = 'B' 넣고 모든 값에 대해 print 했지만 우리는 사용자가 넣은 파일에 대해서만 print
+	# mb를 처음부터 넣지 않을 수도 있음
 	if int(l[0]) == 1:
 		mb = 'M'
 	
-	  result_string=mb+', '+str(minus_list).strip('[]')+', '+file_name
-	  print(result_string)
+	 	result_string=mb+', '+str(minus_list).strip('[]')+', '+file_name
+	 	print(result_string)
