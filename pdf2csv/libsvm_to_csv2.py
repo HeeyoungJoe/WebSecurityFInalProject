5a42ff96d13a8985f241e667044ec99adba31537
abcsm1004 <94301058+abcsm1004@users.noreply.github.com>
2021-12-01 15:25:45 +0900

    Add files via upload

--- pdf2csv/libsvm_to_csv2.py
+++ pdf2csv/libsvm_to_csv2.py
@@ -3,23 +3,20 @@ f = open('./data.libsvm', 'r')
 
 lines = f.readlines()
 for line in lines:
-	
-	minus_list = [-1]*275
-	
+
+	minus_list = [-1]*142
+
 	l = line.split(' #')[0]
 	l = l.split(' ')
 	file_name = line.split(' #')[1].strip('\n')
-	
+
 	for i in range(len(l)):
 		if i != 0:
 			key = int(l[i].split(':')[0])
 			val = float(l[i].split(':')[1])
-			minus_list[key-1]=val
-			
-	# 교수님 코드에서는 if l[0]값에 따라 mb = 'M' or mb = 'B' 넣고 모든 값에 대해 print 했지만 우리는 사용자가 넣은 파일에 대해서만 print
-	# mb를 처음부터 넣지 않을 수도 있음
+			minus_list[key-1] = val
 	if int(l[0]) == 1:
 		mb = 'M'
-	
-	 	result_string=mb+', '+str(minus_list).strip('[]')+', '+file_name
-	 	print(result_string)
+
+		result_string = mb+', '+str(minus_list).strip('[]')+', '+file_name
+		print(result_string)	
