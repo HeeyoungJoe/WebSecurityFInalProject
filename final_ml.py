79e5cabee44150649a8f248d23debc0ab66d2cde
HeeyoungJoe <heeyoungjoe2000@gmail.com>
2021-12-04 23:53:15 +0900

    04_2354 JH

--- final_ml.py
+++ final_ml.py
@@ -2,10 +2,13 @@ import sys
 sys.path.insert(1, './')
 from ml import final,print_result
 import pandas as pd
+import pickle
 
 trainpath="./pdf2csv/output.csv"
 testpath=None
 rf_final,name=final(trainpath)#모델
-testdata=pd.read_csv(testpath).to_numpy()
-pred=rf_final.predict(column_slice(testdata))
-print_result(pred,name)
+
+filename="finalized_model.sav"
+pickle.dump(rf_final,open(filename,'wb'))
+#load model later
+#model=pickle.load(open(filename,'rb'))
---
+++ pdf2csv/test.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+data=pd.read_csv('index_filename.csv')
